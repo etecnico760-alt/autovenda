f4d3246d08547e869b9fe46aa2baf91dd5c3634f
etecnico760-alt <etecnico760@gmail.com>
2026-05-05 18:49:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/testar", async (req, res) => {
   const mensagem = req.query.msg || "Olá!";
   try {
     const apiKey = process.env.GEMINI_API_KEY;
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent:generateContent?key=${apiKey}`;
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent:generateContent?key=${apiKey}`;
     const response = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
